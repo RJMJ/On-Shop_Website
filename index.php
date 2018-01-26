f0795637d4aa3e5704da79fd60560c68bb6e3dab
S m Adil <adilsheik98@gmail.com>
2018-01-26 23:54:45 +0530

    Update index.php

--- index.php
+++ index.php
@@ -16,17 +16,54 @@ include 'config.php';
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="include/bootstrap/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700">
     <link rel="stylesheet" type="text/css" href="include/style.css">
     <link rel="stylesheet" type="text/css" href="include/display.css">
 </head>
 
 <body>
 
-   <!-- Include header.php -->
-   <?php
-      include "header.php";
-   ?><br>
+<!-- Header Nav ============================================================== -->
+  <div class="header-blue">
+    <nav class="navbar navbar-default navigation-clean-search">
+      <div class="container">
+        <div class="navbar-header"><a class="navbar-brand navbar-link" href="index.php">On-Shop</a>
+                    <button class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button>
+        </div>
+        <div class="collapse navbar-collapse" id="navcol-1">
+          <ul class="nav navbar-nav">
+            <li class="active" role="presentation"><a href="index.php">Home</a></li>
+            <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" aria-expanded="false" href="#">All Catagories <span class="caret"></span></a>
+              <ul class="dropdown-menu" role="menu">
+                <li><a href="product.php">Smart Phones</a></li>
+                <li><a href="product.php">Laptops</a></li>
+                <li><a href="product.php">Accessories</a></li>
+              </ul>
+            </li>
+          </ul>
+          <form class="navbar-form navbar-left" target="_self" method="GET" action="search.php">
+            <div class="form-group">
+              <label class="control-label" for="search-field"><i class="glyphicon glyphicon-search"></i></label>
+                <input class="form-control search-field" type="search" name="search" id="search-field">
+            </div>
+          </form>
+          <p class="navbar-text navbar-right">
+            <?php
+              if(isset($_SESSION['username'])){
+                echo '<a  class="navbar-link login" href="cart.php"><i class="glyphicon glyphicon-shopping-cart"></i> Cart</a>';
+                echo '<a class="navbar-link login" href="logout.php"><i class="glyphicon glyphicon -log-out"></i> Log Out</a>';
+                echo '<a class="btn btn-default action-button" href="account.php"><i class="glyphicon glyphicon-user"></i> My Account</a>';
+              }
+              else {
+                echo '<a class="navbar-link login" href="login.php"><i class="glyphicon glyphicon glyphicon-user"></i> Log In</a>';
+                echo '<a class="btn btn-default action-button" href="signup.php"><i class="glyphicon glyphicon-log-in"></i> Sign Up</a>';
+              }
+            ?>
+          </p>
+        </div>
+      </div>
+     </nav>
+    </div>
+<!-- Header Nav End --><br>
 
    <!-- Carousal ================================================================ -->
 	  <div id="myCarousel" class="carousel slide" data-ride="carousel">
@@ -182,8 +219,8 @@ include 'config.php';
                   </div>
                </div>
             </div>
-   
          <!--======= Navigation Buttons =========-->
+
       <!-- Left and right controls -->
       <a class="left carousel-control" href="#adv_team_4_columns_carousel" role="button" data-slide="prev">
         <span class="glyphicon glyphicon-chevron-left"></span>
@@ -197,88 +234,98 @@ include 'config.php';
    </div>
    </div>
    <div class="col-md-6">
-
    </div>
 </div>
 <!-- Slider Product End -->
 
 <!-- Index Product ============================================================ -->
-
       <div class="container">
          <div class="row team_columns_carousel_wrapper">
-            <?php
-
+          <?php
           $sql = $mysqli->query("SELECT * FROM products");
-
           if($sql){
-              
               while($obj = $sql->fetch_object()) {
-               echo '
-               <div class="item">
-               <div class="col-xs-12 col-sm-6 col-md-3 team_columns_item_image">
-                  <a><img src="assets/img/product/'.$obj->product_img_name.'" width="100%" data-toggle="modal" data-target="#'.$obj->id.'"></a>    
-                  <div class="team_columns_item_caption">
-                     <h5 data-toggle="modal" data-target="#'.$obj->id.'"><a>'.$obj->product_name.'</a></h5>
-                     <hr>
-                     <h5>₹ '.$obj->price.'</h5>
-                  </div>         
-               </div>
-               </div>';
-               echo '
-               <div class="modal fade" id="'.$obj->id.'" role="dialog">
-   				 <div class="modal-dialog modal-lg">
-      				<div class="modal-content">
-			        	<div class="modal-header">
-			          		<button type="button" class="close" data-dismiss="modal">&times;</button>
-			          		<h4 class="modal-title">'.$obj->product_name.'</h4>
-			        	</div>';
-			       echo '<div class="modal-body">';
-			   //    <div class="container">
-						// <div class="card">
-						// <div class="wrapper row">
-						// <div class="preview col-md-8">
-						// 	<div class="preview-pic tab-content">
-						// 	  <div class="tab-pane active" id="pic-1"><img src="assets/img/product/'.$obj->product_img_name.'" /></div>
-						// 	</div>
-						// </div>
-						// <div class="details col-md-8">
-						// 	<h3 class="product-title">Product Display</h3>
-			   //            	<p><h3>'.$obj->product_name.'</h3></p>
-      //                   	<p><h4><strong>Description</strong>: '.$obj->product_desc.'</h4></p>
-      //                   	<p><h3><strong>Price (Per Unit)</strong>: '.$currency.$obj->price.'</h3></p>
-						// 	<p class="vote"><h4><strong>91%</strong> of buyers enjoyed this product! <strong>(87 votes)</strong></h4></p>
-						// 	<h5 class="colors">colors:
-						// 	<span class="color orange not-available" data-toggle="tooltip" title="Not In store"></span>
-						// 	<span class="color green"></span>
-						// 	<span class="color blue"></span><br><br><br>		
-			   //              <p><strong>Units Available</strong>: '.$obj->qty.'</p><br>';
-			                
-                         echo '<div class="col-lg-4"><img src="assets/img/product/'.$obj->product_img_name.'"></div>
-                               <p><h3>'.$obj->product_name.'</h3></p>
-                               <p><h4><strong>Description</strong>: '.$obj->product_desc.'</h4></p>
-                               <p><h3><strong>Price (Per Unit)</strong>: '.$currency.$obj->price.'</h3></p>
-                               <p><strong>Units Available</strong>: '.$obj->qty.'</p><br>';
-                 if($obj->qty > 0){
-	                	echo '<p><a href="update-cart.php?action=add&id='.$obj->id.'"><input type="submit" class="add-to-cart" value="Add To Cart" style=";">';
-				        echo '</div></div>';
-			        }
-			        else {
-						echo 'Out Of Stock!';
-					}
-			            //echo '</div></div>';
-				echo '</div></div>';
-            
-            }
-         }
-         ?>
+              echo '<div class="item">
+                    	<div class="col-xs-12 col-sm-6 col-md-3 team_columns_item_image">
+                        <a><img src="assets/img/product/'.$obj->product_img_name.'" width="100%" data-toggle="modal" data-target="#'.$obj->id.'"></a>    
+                        <div class="team_columns_item_caption">
+                        <h5 data-toggle="modal" data-target="#'.$obj->id.'"><a>'.$obj->product_name.'</a></h5>
+                        <hr>
+                        <h5>₹ '.$obj->price.'</h5>
+                        </div>         
+                      </div>
+                    </div>';
+              echo '<div class="modal fade" id="'.$obj->id.'" role="dialog">
+     			<div class="modal-dialog modal-lg">
+        		<div class="modal-content">
+  			<div class="modal-header">
+  			<button type="button" class="close" data-dismiss="modal">&times;</button>
+  		        <h4 class="modal-title">'.$obj->product_name.'</h4>
+		    </div>';
+	      echo '<div class="modal-body">';              
+              echo '<div class="col-lg-4"><img src="assets/img/product/'.$obj->product_img_name.'"></div>
+                    <p><h3>'.$obj->product_name.'</h3></p>
+                    <p><h4><strong>Description</strong>: '.$obj->product_desc.'</h4></p>
+                    <p><h3><strong>Price (Per Unit)</strong>: '.$currency.$obj->price.'</h3></p>
+                    <p><strong>Units Available</strong>: '.$obj->qty.'</p><br>';
+                    if($obj->qty > 0){
+    	               	echo '<p><a href="update-cart.php?action=add&id='.$obj->id.'"><input type="submit" class="add-to-cart" value="Add To Cart" style=";">';
+  		        echo '</div></div>';
+    	             }
+    	            else {
+  		        echo 'Out Of Stock!';
+    		    }
+       	      echo '</div></div>';            
+              }
+          }?>
          <hr></div>
       </div><br>
 <!-- Index Product End -->
 
-	<!-- Include footer.php -->
-   <?php
-      include "footer.php";
-   ?>
+<!-- Include footer.php -->
+    <footer id="myFooter">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-3 myCols">
+          <h5>Get started</h5>
+          <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="signup.php">Sign up</a></li>
+            <li><a href="login.php">Log In</a></li>
+          </ul>
+        </div>
+        <div class="col-sm-3 myCols">
+          <h5>About us</h5>
+            <ul>
+              <li><a href="about.php">Company Information</a></li>
+              <li><a href="contact.php">Contact us</a></li>
+              <li><a href="rev.php">Reviews</a></li>
+            </ul>
+        </div>
+        <div class="col-sm-3 myCols">
+          <h5>Support</h5>
+            <ul>
+              <li><a href="faq.php">FAQ</a></li>
+              <li><a href="contact.php">Help desk</a></li>
+              <li><a href="index.php">Forums</a></li>
+            </ul>
+        </div>
+        <div class="col-sm-3 myCols">
+          <h5>Legal</h5>
+            <ul>
+              <li><a href="tns.php">Terms of Service</a></li>
+              <li><a href="tns.php">Terms of Use</a></li>
+              <li><a href="tns.php">Privacy Policy</a></li>
+            </ul>
+        </div>
+      </div>
+    </div><hr>    
+    <div class="footer-copyright">
+      <p><a href="admin_login.php">© 2017-2018 Copyright by On-Shop, All rights reserved</a></p>
+        </div>
+    </footer>
+<!-- Footer End -->
+
     <script src="include/bootstrap/js/jquery-3.2.1.min.js"></script>
     <script src="include/bootstrap/js/bootstrap.min.js"></script>
 </body>
